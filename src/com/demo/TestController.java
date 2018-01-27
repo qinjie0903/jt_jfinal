dc3b9e504676b12d5b08f3da1b45b1d57bf90333
qinjie <qinjie0903@126.com>
2018-01-27 17:26:53 +0800

    2.3 JFinal控制器详解_路由详解

--- src/com/demo/TestController.java
+++ src/com/demo/TestController.java
@@ -8,7 +8,12 @@ public class TestController extends Controller{
 		//renderText("test Page.");
 		render("test.html");
 		
-		//如果是/開頭自動在WebRoot目錄下尋找
-		//如果沒有的話就按具體的路徑尋找
+		//如果是/開頭自動在WebRoot目錄下尋找  
+		//me.add("/test", TestController.class, "test/index"); 最后一个参数的配置则没有效果
+	
+		
+		//如果沒有的話就按具體的路徑尋找 
+		//me.add("/test", TestController.class, "test/index"); 最后一个参数的配置则有效果
+
 	}
 }
